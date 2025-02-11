f6080c31a0bbcbc3ea8734cd88832839e0c0aaec
Jesus Coronado <jesusalfredocoronado@gmail.com>
2025-02-11 14:29:28 -0500

    V1.5.8.3

--- app/routes/os_routes.py
+++ app/routes/os_routes.py
@@ -48,6 +48,6 @@ async def asignar_tecnico_os(os:OST):
     return rpta
 
 @router.put("/update_os")
-async def update_os(os:OSUpdate):
-    rpta= nueva_os.update_os(os)
+async def update_os(os:OSUpdate, pos: pendiente_os):
+    rpta= nueva_os.update_os(os, pos)
     return rpta
